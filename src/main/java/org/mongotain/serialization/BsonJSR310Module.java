db24547be17129663c6d2b614b4b6aaed415ec0a
driccio <damien.riccio@4sh.fr>
2018-06-01 16:10:37 +0200

    refacto: Use jongo 1.4.0

--- src/main/java/org/mongotain/serialization/BsonJSR310Module.java
+++ src/main/java/org/mongotain/serialization/BsonJSR310Module.java
@@ -1,13 +1,13 @@
 package org.mongotain.serialization;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import de.undercouch.bson4jackson.BsonGenerator;
-import de.undercouch.bson4jackson.serializers.BsonSerializer;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -20,41 +20,45 @@ public class BsonJSR310Module extends SimpleModule {
 
         super("BsonJSR310Module");
 
-        addSerializer(Instant.class, new BsonSerializer<Instant>() {
+        addSerializer(Instant.class, new JsonSerializer<Instant>() {
             @Override
-            public void serialize(Instant date, BsonGenerator bsonGenerator, SerializerProvider serializerProvider)
+            public void serialize(Instant date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
                     throws IOException {
                 if (date == null) {
-                    serializerProvider.defaultSerializeNull(bsonGenerator);
+                    serializerProvider.defaultSerializeNull(jsonGenerator);
                 } else {
-                    bsonGenerator.writeDateTime(new Date(date.toEpochMilli()));
+                    if (jsonGenerator instanceof BsonGenerator) {
+                        ((BsonGenerator) jsonGenerator).writeDateTime(new Date(date.toEpochMilli()));
+                    }
                 }
             }
         });
 
         addDeserializer(Instant.class, new JsonDeserializer<Instant>() {
             @Override
-            public Instant deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            public Instant deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
                 Date date = (Date) jp.getEmbeddedObject();
                 return Instant.ofEpochMilli(date.getTime());
             }
         });
 
-        addSerializer(LocalDate.class, new BsonSerializer<LocalDate>() {
+        addSerializer(LocalDate.class, new JsonSerializer<LocalDate>() {
             @Override
-            public void serialize(LocalDate date, BsonGenerator bsonGenerator, SerializerProvider serializerProvider)
+            public void serialize(LocalDate date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
                     throws IOException {
                 if (date == null) {
-                    serializerProvider.defaultSerializeNull(bsonGenerator);
+                    serializerProvider.defaultSerializeNull(jsonGenerator);
                 } else {
-                    bsonGenerator.writeDateTime(new Date(date.toEpochDay()));
+                    if (jsonGenerator instanceof BsonGenerator) {
+                        ((BsonGenerator) jsonGenerator).writeDateTime(new Date(date.toEpochDay()));
+                    }
                 }
             }
         });
 
         addDeserializer(LocalDate.class, new JsonDeserializer<LocalDate>() {
             @Override
-            public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
                 Date date = (Date) jp.getEmbeddedObject();
                 return LocalDate.ofEpochDay(date.getTime());
             }
--- src/main/java/org/mongotain/serialization/PathJsonModule.java
+++ src/main/java/org/mongotain/serialization/PathJsonModule.java
@@ -1,13 +1,12 @@
 package org.mongotain.serialization;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import de.undercouch.bson4jackson.BsonGenerator;
-import de.undercouch.bson4jackson.serializers.BsonSerializer;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -19,21 +18,21 @@ public class PathJsonModule extends SimpleModule {
 
         super("PathJsonModule");
 
-        addSerializer(Path.class, new BsonSerializer<Path>() {
+        addSerializer(Path.class, new JsonSerializer<Path>() {
             @Override
-            public void serialize(Path path, BsonGenerator bsonGenerator, SerializerProvider serializerProvider)
+            public void serialize(Path path, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
                     throws IOException {
                 if (path == null) {
-                    serializerProvider.defaultSerializeNull(bsonGenerator);
+                    serializerProvider.defaultSerializeNull(jsonGenerator);
                 } else {
-                    bsonGenerator.writeString(path.toString());
+                    jsonGenerator.writeString(path.toString());
                 }
             }
         });
 
         addDeserializer(Path.class, new JsonDeserializer<Path>() {
             @Override
-            public Path deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            public Path deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
                 String path = (String) jp.getEmbeddedObject();
                 return Paths.get(path);
             }
